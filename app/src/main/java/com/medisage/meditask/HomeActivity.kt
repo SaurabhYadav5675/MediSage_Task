cd600e3d589c491e5a5e3309669f14f00548f7e1
TeachUs <51109175+thinkmonk@users.noreply.github.com>
2022-10-16 17:36:02 +0530

    initial commit
    room data insert

--- app/src/main/java/com/medisage/meditask/HomeActivity.kt
+++ app/src/main/java/com/medisage/meditask/HomeActivity.kt
@@ -1,5 +1,6 @@
 package com.medisage.meditask
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.medisage.meditask.api.ApiService
@@ -10,6 +11,7 @@ import com.medisage.meditask.fragment.FavouriteFragment
 import com.medisage.meditask.fragment.FragmentAdapter
 import com.medisage.meditask.fragment.PostFragment
 import com.medisage.meditask.repository.PostRepository
+import com.medisage.meditask.utilities.AppPreference
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
@@ -27,6 +29,16 @@ class HomeActivity : AppCompatActivity() {
 
         var viewPager = binding.homeViewPager
         var tabLayout = binding.homeTabs
+        var toolbarTitle = binding.toolbar.toolbarTitleText
+        var logout = binding.toolbar.toolbarLogout
+
+        toolbarTitle.text = "Home"
+        logout.setOnClickListener {
+            AppPreference.setLoginStatus(this, 0)
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
 
         val fragmentAdapter = FragmentAdapter(supportFragmentManager)
         fragmentAdapter.addFragment(PostFragment(), "Post")
